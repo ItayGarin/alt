12063df011b4e00eee6cdc988d031d626f789a1e
Itay Garin <thifixp@gmail.com>
2020-06-08 22:33:06 +0300

    Gonna give up on async zmq

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,14 @@ use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
+use tokio::sync::Mutex;
 // use tokio::prelude::*;
+
+use std::sync::Arc;
+
+use tmq::request_reply::RequestSender;
+use tmq::{request, Context};
+// use futures::SinkExt;
 
 #[derive(Copy, Clone, Debug)]
 pub enum EvState {
@@ -32,79 +39,135 @@ pub struct Event<'a> {
     state: EvState,
 }
 
-async fn handle_event(event: Event<'_>) -> Result<(), Box<dyn std::error::Error>> {
-    Ok(())
+struct Alt {
+    context: Context,
 }
 
-async fn handle_buf(buf: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
-    let str_buf = std::str::from_utf8(buf)?;
-    let split: Vec<&str> = str_buf.split(":").collect();
+type ArcAlt = Arc<Mutex<Alt>>;
+type DynError = Box<dyn std::error::Error>;
 
-    if split.len() != 2 {
-        let err = Box::new(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "Bad event (should be <name>:<on/off>)",
-        ));
-        return Err(err);
+impl Alt {
+    pub async fn new() -> Result<Self, DynError> {
+        let context = Context::new();
+
+        Ok(Alt {
+            context,
+        })
     }
 
-    let name = split[0].trim();
-    let state = EvState::from_str(split[1].trim())?;
-    let event = Event{name, state};
+    pub async fn init_listen_socket() -> Result<TcpListener, DynError> {
+        let endpoint = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 7332);
+        let listen_socket = TcpListener::bind(endpoint).await?;
+        Ok(listen_socket)
+    }
 
-    dbg!(&event);
-    handle_event(event).await
-}
+    async fn handle_ivy(ktrl_socket: RequestSender, event: Event<'_>) -> Result<RequestSender, DynError> {
+        let msg = tmq::Message::from("IpcDoEffect((fx: NoOp, val: Press))");
+        let multipart = tmq::Multipart::from(msg);
+        let reciver = ktrl_socket.send(multipart).await?;
+        Ok(())
+    }
 
-async fn handle_conn(mut conn: TcpStream) {
-    let mut buf: [u8; 1024] = [0; 1024];
+    async fn handle_event(ktrl_socket: RequestSender, event: Event<'_>) -> Result<RequestSender, DynError> {
+        match event.name {
+            "ivy" => {
+                Self::handle_ivy(ktrl_socket, event).await?;
+                ()
+            }
+            _ => (),
+        }
 
-    // In a loop, read data from the socket and write the data back.
-    loop {
-        let is_ok = match conn.read(&mut buf).await {
-            // socket closed
-            Ok(n) if n == 0 => {
-                println!("Client exited");
-                return;
-            },
-            Ok(n) => {
-                if let Err(e) = handle_buf(&buf[0..n]).await {
-                    eprintln!("client encountered an error; err = {:?}", &e);
-                    false
-                } else {
-                    true
+        Ok(())
+    }
+
+    async fn handle_buf(ktrl_socket: RequestSender, buf: &[u8]) -> Result<RequestSender, DynError> {
+        let str_buf = std::str::from_utf8(buf)?;
+        let split: Vec<&str> = str_buf.split(":").collect();
+
+        if split.len() != 2 {
+            let err = Box::new(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "Bad event (should be <name>:<on/off>)",
+            ));
+            return Err(err);
+        }
+
+        let name = split[0].trim();
+        let state = EvState::from_str(split[1].trim())?;
+        let event = Event { name, state };
+
+        dbg!(&event);
+        Self::handle_event(ktrl_socket, event).await
+    }
+
+    async fn handle_conn(alt: &ArcAlt, mut conn: TcpStream) {
+        let mut buf: [u8; 1024] = [0; 1024];
+
+        let mut ktrl_socket = {
+            let alt = alt.lock().await;
+            match request(&alt.context).connect("tcp://127.0.0.1:7331") {
+                Ok(socket) => socket,
+                Err(e) => {
+                    println!("Failed to connect to ktrl");
+                    return;
                 }
             }
-            Err(e) => {
-                eprintln!("failed to read from socket; err = {:?}", e);
-                false
+        };
+
+        // In a loop, read data from the socket and write the data back.
+        loop {
+            let is_ok = match conn.read(&mut buf).await {
+                // socket closed
+                Ok(n) if n == 0 => {
+                    println!("Client exited");
+                    return;
+                }
+                Ok(n) => {
+                    let res = Self::handle_buf(ktrl_socket, &buf[0..n]).await;
+                    if let Err(e) = res {
+                        eprintln!("client encountered an error; err = {:?}", &e);
+                        false
+                    } else {
+                        ktrl_socket = res.unwrap();
+                        true
+                    }
+                }
+                Err(e) => {
+                    eprintln!("failed to read from socket; err = {:?}", e);
+                    false
+                }
+            };
+
+            let reply: &[u8] = match is_ok {
+                true => b"OK\n",
+                _ => b"ERROR\n",
+            };
+
+            if let Err(e) = conn.write_all(reply).await {
+                eprintln!("Failed to send a reply; err = {:?}", e);
+                return;
             }
-        };
+        }
+    }
 
-        let reply: &[u8] = match is_ok {
-            true => b"OK\n",
-            _ => b"ERROR\n",
-        };
-
-        if let Err(e) = conn.write_all(reply).await {
-            eprintln!("Failed to send a reply; err = {:?}", e);
-            return;
+    pub async fn event_loop(self) -> Result<(), DynError> {
+        let mut listen_socket = Self::init_listen_socket().await?;
+        let arc_alt = Arc::new(Mutex::new(self));
+        loop {
+            let (socket, _) = listen_socket.accept().await?;
+            let arc_clone = arc_alt.clone();
+            tokio::spawn(async move {
+                println!("A new client connected");
+                Self::handle_conn(&arc_clone, socket).await;
+            });
         }
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), DynError> {
     println!("ALT: Started");
-
-    let endpoint = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 7332);
-    let mut listener = TcpListener::bind(endpoint).await?;
-
-    loop {
-        let (socket, _) = listener.accept().await?;
-        tokio::spawn(async move {
-            println!("A new client connected");
-            handle_conn(socket).await;
-        });
-    }
+    let alt = Alt::new().await?;
+    alt.event_loop().await?;
+    Ok(())
 }
